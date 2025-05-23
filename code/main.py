e6c9b3a1ded3266b90a2d1c2e42b23d2a94caba6
Lucas Costa Barbosa <lucas.barbosa.0899@poli.ufrj.br>
2025-05-23 20:07:11 -0300

    found minimal parameters for finding best recipe

--- code/main.py
+++ code/main.py
@@ -12,7 +12,7 @@ from simulate import ChainSimulation
 chain = ChainSimulation()
 results_data, results_opt = chain.optimize_recipes(
     base_product="OG Kush",
-    batch_size=1000,
+    batch_size=10_000,
     n_steps=300,
     recipe_size=8,
     initial_temperature=1.0,
@@ -29,26 +29,25 @@ Lucro: {results_opt["profit"]}
 )
 
 # Plot results
-plot_final_step_ingredients_barplot(
-    recipes=results_data['recipes'],
-    ingredients_name=chain.ingredients_df['ingredient_name'].tolist(),
-)
-plot_ingredients_lineplot(
-    recipes=results_data['recipes'],
-    ingredients_name=chain.ingredients_df['ingredient_name'].tolist(),
-)
+# plot_final_step_ingredients_barplot(
+#     recipes=results_data['recipes'],
+#     ingredients_name=chain.ingredients_df['ingredient_name'].tolist(),
+# )
+# plot_ingredients_lineplot(
+#     recipes=results_data['recipes'],
+#     ingredients_name=chain.ingredients_df['ingredient_name'].tolist(),
+# )
 
-plot_effects_lineplot(
-    effects=results_data['effects'],
-    effects_name=chain.effects_df['effect_name'].tolist(),
-)
+# plot_effects_lineplot(
+#     effects=results_data['effects'],
+#     effects_name=chain.effects_df['effect_name'].tolist(),
+# )
 
-plot_profit_lineplot(
-    profits=results_data['profits'],
-)
+# plot_profit_lineplot(
+#     profits=results_data['profits'],
+# )
 
 # plot_recipes_sankey(
-#     profits=results_data['profits'],
 #     recipes=results_data['recipes'],
 #     ingredients_name=chain.ingredients_df['ingredient_name'].tolist(),
 # )
